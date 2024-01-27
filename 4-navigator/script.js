363c6924536905685108b4bef261c908204bcf40
Ilya Algazin <yanejr8@gmail.com>
2024-01-27 14:46:39 -0200

    Revert "first code-review"
    This reverts commit 851966f49fd633c95548e001b8d3f804a7a6c983.

--- 4-navigator/script.js
+++
@@ -1,7 +0,0 @@
-adressLat = 10;
-adressLong = 7;
-positionLat = 20;
-positionLong = 23;
-
-distance = (adressLat - positionLat)**2 + (adressLong- positionLong)**2;
-console.log(distance)
--- 6-driver/driver.js
+++
@@ -1,5 +0,0 @@
-const age = 18;
-const isDrunk = false;
-const hasLicence = true;
-
-console.log(`Может ли пользоваетль вести машину: ${age >= 18 && !isDrunk && hasLicence ? 'может':'не может'}`)
